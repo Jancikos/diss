79337bb0ba7b81d8637f4388cc6c7b706433b729
Ján Kostor <ko.janci@gmail.com>
2025-03-02 11:37:01 +0100

    SP1 gui intit

---
+++ FRI.DISS/FRI.DISS.SP1/BusinessmanJanSimulationElement.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FRI.DISS.SP1
+{
+    /// <summary>
+    /// Interaction logic for BusinessmanJanSimulationElement.xaml
+    /// </summary>
+    public partial class BusinessmanJanSimulationElement : UserControl
+    {
+        public string Title
+        {
+            get { return _txt_Title.Header.ToString() ?? ""; }
+            set { _txt_Title.Header = value; }
+        }
+
+        public BusinessmanJanSimulationElement()
+        {
+            InitializeComponent();
+        }
+    }
+}
