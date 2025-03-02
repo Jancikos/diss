ba778bb8b42c88db59d7864ae1279edae6c2fccb
Ján Kostor <ko.janci@gmail.com>
2025-03-02 15:07:07 +0100

    sp1 rendering multiple values into one plot

--- FRI.DISS/FRI.DISS.SP1/BusinessmanJanSimulationElement.xaml.cs
+++ FRI.DISS/FRI.DISS.SP1/BusinessmanJanSimulationElement.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Versioning;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -29,7 +30,15 @@ namespace FRI.DISS.SP1
         public BusinessmanJan? Simulation { get; set; }
 
         public int SkipFirstCount = 0;
-        protected DataLogger? _dataLogger;
+        protected DataLogger? _dataLoggerCosts;
+        protected IYAxis? _yAxisCosts;
+        protected DataLogger? _dataLoggerSuppliersReliability;
+        protected IYAxis? _yAxisSuppliersReliability;
+        protected DataLogger? _dataLoggerWerehousesItemsLeft;
+        protected IYAxis? _yAxisWerehousesItemsLeft;
+        protected DataLogger? _dataLoggerMissingDemandItemsCount;
+        protected IYAxis? _yAxisMissingDemandItemsCount;
+
         protected Stopwatch _stopwatch = new();
 
         public string Title
@@ -48,7 +57,31 @@ namespace FRI.DISS.SP1
         {
             InitializeComponent();
 
+            _initializePlot();
             _clearGUI();
+        }
+
+        private void _initializePlot()
+        {
+            _yAxisCosts = _plot.Plot.Axes.Left;
+            _yAxisCosts.Label.Text = "Total costs";
+            _yAxisCosts.Label.FontSize = 12;
+            _yAxisCosts.Label.ForeColor = ScottPlot.Color.FromColor(System.Drawing.Color.Black);
+
+            _yAxisSuppliersReliability = _plot.Plot.Axes.AddLeftAxis();
+            _yAxisSuppliersReliability.Label.Text = "Suppliers Reliability";
+            _yAxisSuppliersReliability.Label.FontSize = 12;
+            _yAxisSuppliersReliability.Label.ForeColor = ScottPlot.Color.FromColor(System.Drawing.Color.Blue);
+
+            _yAxisWerehousesItemsLeft = _plot.Plot.Axes.AddRightAxis();
+            _yAxisWerehousesItemsLeft.Label.Text = "Werehouses Items Left";
+            _yAxisWerehousesItemsLeft.Label.FontSize = 12;
+            _yAxisWerehousesItemsLeft.Label.ForeColor = ScottPlot.Color.FromColor(System.Drawing.Color.Orange);
+
+            _yAxisMissingDemandItemsCount = _plot.Plot.Axes.AddRightAxis();
+            _yAxisMissingDemandItemsCount.Label.Text = "Missing Demand Items";
+            _yAxisMissingDemandItemsCount.Label.FontSize = 12;
+            _yAxisMissingDemandItemsCount.Label.ForeColor = ScottPlot.Color.FromColor(System.Drawing.Color.Red);
         }
 
         public void StartSimulation()
@@ -57,14 +90,28 @@ namespace FRI.DISS.SP1
             {
                 throw new InvalidOperationException("Simulation not set");
             }
-
             Dispatcher.Invoke(_clearGUI);
 
-            _dataLogger = _plot.Plot.Add.DataLogger();
+            _plot.Plot.Clear();
+
+            _dataLoggerCosts = _plot.Plot.Add.DataLogger();
+            _dataLoggerCosts.Axes.YAxis = _yAxisCosts!;
+            _dataLoggerCosts.Color = _yAxisCosts!.Label.ForeColor;
+
+            _dataLoggerSuppliersReliability = _plot.Plot.Add.DataLogger();
+            _dataLoggerSuppliersReliability.Axes.YAxis = _yAxisSuppliersReliability!;
+            _dataLoggerSuppliersReliability.Color = _yAxisSuppliersReliability!.Label.ForeColor;
+
+            _dataLoggerWerehousesItemsLeft = _plot.Plot.Add.DataLogger();
+            _dataLoggerWerehousesItemsLeft.Axes.YAxis = _yAxisWerehousesItemsLeft!;
+            _dataLoggerWerehousesItemsLeft.Color = _yAxisWerehousesItemsLeft!.Label.ForeColor;
+
+            _dataLoggerMissingDemandItemsCount = _plot.Plot.Add.DataLogger();
+            _dataLoggerMissingDemandItemsCount.Axes.YAxis = _yAxisMissingDemandItemsCount!;
+            _dataLoggerMissingDemandItemsCount.Color = _yAxisMissingDemandItemsCount!.Label.ForeColor;
 
             var axis = (RightAxis)_plot.Plot.Axes.Right;
-            axis.Color(_dataLogger.Color);
-
+            axis.Color(_dataLoggerCosts.Color);
 
             Simulation.UpdateStatsCallback = (mc, repDone, resultRaw) =>
             {
@@ -78,7 +125,11 @@ namespace FRI.DISS.SP1
                     _updateStats();
 
                     // update plot
-                    _dataLogger.Add(repDone, Simulation.ProcessExperimentResult());
+                    _dataLoggerCosts!.Add(repDone, Simulation.ProcessExperimentResult());
+                    _dataLoggerSuppliersReliability!.Add(repDone, Simulation.ResultSuplliersReliability!.Mean);
+                    _dataLoggerWerehousesItemsLeft!.Add(repDone, Simulation.ResultWarehouseItemsLeftCount!.Mean);
+                    _dataLoggerMissingDemandItemsCount!.Add(repDone, Simulation.ResultMissingDemandItemsCount!.Mean);
+
                     _plot.Refresh();
                 });
             };
