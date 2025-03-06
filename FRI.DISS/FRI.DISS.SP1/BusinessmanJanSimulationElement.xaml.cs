dbc8d6bb76444344d77f07e761032488c3a6fd0c
Ján Kostor <ko.janci@gmail.com>
2025-03-06 20:35:32 +0100

    sp1: removing multiple plot y-axis

--- FRI.DISS/FRI.DISS.SP1/BusinessmanJanSimulationElement.xaml.cs
+++ FRI.DISS/FRI.DISS.SP1/BusinessmanJanSimulationElement.xaml.cs
@@ -32,12 +32,6 @@ namespace FRI.DISS.SP1
         public int SkipFirstCount = 0;
         protected DataLogger? _dataLoggerCosts;
         protected IYAxis? _yAxisCosts;
-        protected DataLogger? _dataLoggerSuppliersReliability;
-        protected IYAxis? _yAxisSuppliersReliability;
-        protected DataLogger? _dataLoggerWerehousesItemsLeft;
-        protected IYAxis? _yAxisWerehousesItemsLeft;
-        protected DataLogger? _dataLoggerMissingDemandItemsCount;
-        protected IYAxis? _yAxisMissingDemandItemsCount;
 
         protected Stopwatch _stopwatch = new();
 
@@ -66,22 +60,7 @@ namespace FRI.DISS.SP1
             _yAxisCosts = _plot.Plot.Axes.Left;
             _yAxisCosts.Label.Text = "Total costs";
             _yAxisCosts.Label.FontSize = 12;
-            _yAxisCosts.Label.ForeColor = ScottPlot.Color.FromColor(System.Drawing.Color.Black);
-
-            _yAxisSuppliersReliability = _plot.Plot.Axes.AddLeftAxis();
-            _yAxisSuppliersReliability.Label.Text = "Suppliers Reliability";
-            _yAxisSuppliersReliability.Label.FontSize = 12;
-            _yAxisSuppliersReliability.Label.ForeColor = ScottPlot.Color.FromColor(System.Drawing.Color.Blue);
-
-            _yAxisWerehousesItemsLeft = _plot.Plot.Axes.AddRightAxis();
-            _yAxisWerehousesItemsLeft.Label.Text = "Werehouses Items Left";
-            _yAxisWerehousesItemsLeft.Label.FontSize = 12;
-            _yAxisWerehousesItemsLeft.Label.ForeColor = ScottPlot.Color.FromColor(System.Drawing.Color.Orange);
-
-            _yAxisMissingDemandItemsCount = _plot.Plot.Axes.AddRightAxis();
-            _yAxisMissingDemandItemsCount.Label.Text = "Missing Demand Items";
-            _yAxisMissingDemandItemsCount.Label.FontSize = 12;
-            _yAxisMissingDemandItemsCount.Label.ForeColor = ScottPlot.Color.FromColor(System.Drawing.Color.Red);
+            _yAxisCosts.Label.ForeColor = ScottPlot.Color.FromColor(System.Drawing.Color.Orange);
         }
 
         public void StartSimulation()
@@ -97,18 +76,6 @@ namespace FRI.DISS.SP1
             _dataLoggerCosts = _plot.Plot.Add.DataLogger();
             _dataLoggerCosts.Axes.YAxis = _yAxisCosts!;
             _dataLoggerCosts.Color = _yAxisCosts!.Label.ForeColor;
-
-            _dataLoggerSuppliersReliability = _plot.Plot.Add.DataLogger();
-            _dataLoggerSuppliersReliability.Axes.YAxis = _yAxisSuppliersReliability!;
-            _dataLoggerSuppliersReliability.Color = _yAxisSuppliersReliability!.Label.ForeColor;
-
-            _dataLoggerWerehousesItemsLeft = _plot.Plot.Add.DataLogger();
-            _dataLoggerWerehousesItemsLeft.Axes.YAxis = _yAxisWerehousesItemsLeft!;
-            _dataLoggerWerehousesItemsLeft.Color = _yAxisWerehousesItemsLeft!.Label.ForeColor;
-
-            _dataLoggerMissingDemandItemsCount = _plot.Plot.Add.DataLogger();
-            _dataLoggerMissingDemandItemsCount.Axes.YAxis = _yAxisMissingDemandItemsCount!;
-            _dataLoggerMissingDemandItemsCount.Color = _yAxisMissingDemandItemsCount!.Label.ForeColor;
 
             var axis = (RightAxis)_plot.Plot.Axes.Right;
             axis.Color(_dataLoggerCosts.Color);
@@ -126,9 +93,6 @@ namespace FRI.DISS.SP1
 
                     // update plot
                     _dataLoggerCosts!.Add(repDone, Simulation.ProcessExperimentResult());
-                    _dataLoggerSuppliersReliability!.Add(repDone, Simulation.ResultSuplliersReliability!.Mean);
-                    _dataLoggerWerehousesItemsLeft!.Add(repDone, Simulation.ResultWarehouseItemsLeftCount!.Mean);
-                    _dataLoggerMissingDemandItemsCount!.Add(repDone, Simulation.ResultMissingDemandItemsCount!.Mean);
 
                     _plot.Refresh();
                 });
