84d5755236fc3ee53908b87f1a002d4447c6e906
Ján Kostor <ko.janci@gmail.com>
2025-03-07 14:52:43 +0100

    sp1: gui reorgnazition

--- FRI.DISS/FRI.DISS.SP1/MainWindow.xaml.cs
+++ FRI.DISS/FRI.DISS.SP1/MainWindow.xaml.cs
@@ -13,37 +13,73 @@ using FRI.DISS.Libs.MonteCarlo;
 
 namespace FRI.DISS.SP1
 {
+    public enum SimulationsType
+    {
+        DefinedStrategies,
+        KostorStrategies,
+        CustomStrategy
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<BusinessmanJanSimulationElement> _simulationElements = new();
+        List<BusinessmanJanSimulationElement>[] _simulationElements;
+
+        public SimulationsType SimulationsType
+        {
+            get { return (SimulationsType)_cmbx_SimulationType.SelectedItem; }
+        }
+
 
         public MainWindow()
         {
             InitializeComponent();
 
-            _initialize();
+            _simulationElements = new List<BusinessmanJanSimulationElement>[Enum.GetValues(typeof(SimulationsType)).Length];
+
+            _initializeGUI();
         }
 
-        private void _initialize()
-        {   
+        private void _initializeGUI()
+        {
             // init simulation elements
-            _simulationElements.Add(_simStrategyA);
+            _simulationElements[(int)SimulationsType.DefinedStrategies] = new List<BusinessmanJanSimulationElement>(){
+                _simStrategyA,
+                _simStrategyB,
+                _simStrategyC,
+                _simStrategyD
+            };
             _simStrategyA.Simulation = new BusinessmanJanStrategyA();
-
-            _simulationElements.Add(_simStrategyB);
             _simStrategyB.Simulation = new BusinessmanJanStrategyB();
-
-            _simulationElements.Add(_simStrategyC);
             _simStrategyC.Simulation = new BusinessmanJanStrategyC();
-
-            _simulationElements.Add(_simStrategyD);
             _simStrategyD.Simulation = new BusinessmanJanStrategyD();
 
-            _simulationElements.Add(_simStrategyKostor);
-            _simStrategyKostor.Simulation = new BusinessmanJanStrategyKostor();
+            // init kostor strategies
+            _simulationElements[(int)SimulationsType.KostorStrategies] = new List<BusinessmanJanSimulationElement>(){
+                _simStrategyKostorA,
+                _simStrategyKostorB,
+                _simStrategyKostorC
+            };
+
+            _simStrategyKostorA.Simulation = new BusinessmanJanStrategyKostor();
+            _simStrategyKostorB.Simulation = new BusinessmanJanStrategyKostor();
+            _simStrategyKostorC.Simulation = new BusinessmanJanStrategyKostor();
+
+            // init custom strategy
+            _simulationElements[(int)SimulationsType.CustomStrategy] = new List<BusinessmanJanSimulationElement>(){
+                _simStrategyCustom
+            };
+            _simStrategyCustom.Simulation = new BusinessmanJanStrategyKostor();
+
+            
+            // Initialize ComboBox
+            foreach (SimulationsType type in Enum.GetValues(typeof(SimulationsType)))
+            {
+                _cmbx_SimulationType.Items.Add(type);
+            }
+            _cmbx_SimulationType.SelectedIndex = (int)SimulationsType.DefinedStrategies;
         }
 
         private void _mnitem_Close_Click(object sender, RoutedEventArgs e)
@@ -58,8 +94,8 @@ namespace FRI.DISS.SP1
             var renderUpdateStatsInterval = _txtbx_SimulationRenderInterval.IntValue;
             var renderSkipFirst = _txtbx_SimulationRenderSkipFirst.IntValue;
 
-            foreach (var simElement in _simulationElements)
-            {   
+            foreach (var simElement in _simulationElements[(int)SimulationsType])
+            {
                 if (!simElement.IsActive)
                 {
                     continue;
@@ -85,14 +121,15 @@ namespace FRI.DISS.SP1
 
         private void _btn_Simulation_Stop_Click(object sender, RoutedEventArgs e)
         {
-            foreach (var simElement in _simulationElements)
+            foreach (var simElement in _simulationElements[(int)SimulationsType])
             {
-                try {
+                try
+                {
                     if (!simElement.IsActive)
                     {
                         continue;
                     }
-                    
+
                     simElement.StopSimulation();
                 }
                 catch (Exception ex)
@@ -100,6 +137,22 @@ namespace FRI.DISS.SP1
                     MessageBox.Show($"Error stopping simulation {simElement.Title}: {ex.Message}", "Simulation Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
+        }
+
+        private void _cmbx_SimulationType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedType = (SimulationsType)_cmbx_SimulationType.SelectedItem;
+
+            for (int i = 0; i < _simulationElements.Length; i++)
+            {
+                foreach (var simElement in _simulationElements[i])
+                {
+                    simElement.Visibility = i == (int)selectedType 
+                        ? Visibility.Visible
+                        : Visibility.Collapsed;
+                }
+            }
+
         }
     }
 }
