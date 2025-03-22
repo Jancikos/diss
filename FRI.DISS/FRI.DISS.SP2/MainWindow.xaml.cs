269c59a893e2801352d6a2a09be3166062ea183e
Ján Kostor <ko.janci@gmail.com>
2025-03-22 08:56:16 +0100

    sp2 project init

---
+++ FRI.DISS/FRI.DISS.SP2/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using FRI.DISS.Libs.Simulations.EventDriven;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FRI.DISS.SP2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void _mnitem_File_Click(object sender, RoutedEventArgs e)
+        {
+            var sim = new StanicaSimulation();
+        }
+
+        private void _mnitem_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
